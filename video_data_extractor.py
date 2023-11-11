3bb7b9be337a5889d7cf4cf62bd046eea30f085d
Kateryna <ekaterina.polishchuk01@gmail.com>
2023-11-11 19:24:59 +0100

    added function to get all the necessary data from the video and save it to a file

--- video_data_extractor.py
+++ video_data_extractor.py
@@ -1,6 +1,8 @@
+import json
 import yt_dlp
 from yt_dlp.utils import DownloadError
-import json
+
+VIDEO_URL = "https://www.youtube.com/@NicholasRenotte/shorts"
 
 def get_video_info(url, playlist_items=1):
     if playlist_items > 1:
@@ -19,7 +21,6 @@ def get_video_info(url, playlist_items=1):
             )
         except DownloadError:
             return None
-        
     if 'entries' in result:
         list_videos = [video for video in result['entries']]
         video = list_videos
@@ -40,8 +41,22 @@ def get_video_url(video):
     url_video = "https://www.youtube.com/watch?v=" + video['id']
     return url_video
 
-if __name__ == '__main__':
-    video_info = get_video_info("https://www.youtube.com/watch?v=giO92FBk33c")
-    print(video_info)
-    url = get_audio_url(video_info)
-    print(url)
+def get_all_videos_infos(url, playlist_items=1):
+    videos_infos = get_video_info(url, playlist_items)
+    video_data = {}
+    for video_info in videos_infos:
+        title = get_title(video_info)
+        video_data[title] = {}
+        video_data[title]["audio_url"] = get_audio_url(video_info)
+        video_data[title]["video_url"] = get_video_url(video_info)
+    return  video_data
+
+def save_video_info_json(videos):
+    filename = 'video_info.json'
+    with open(filename, 'w', encoding='UTF-8') as file_object:
+        json.dump(videos, file_object)
+    file_object.close()
+
+if __name__ == "__main__":
+    all_videos_infos = get_all_videos_infos(VIDEO_URL, playlist_items=10)
+    save_video_info_json(all_videos_infos)
